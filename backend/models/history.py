4a9509b445441a3e359797ff868747a5beef022e
anawin111909 <108145027+anawin111909@users.noreply.github.com>
2025-05-08 03:54:29 +0100

    update 2.9

--- backend/models/history.py
+++ backend/models/history.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime 
 from database import Base
 
 class SearchHistory(Base):
     __tablename__ = "search_history"
     id = Column(Integer, primary_key=True, index=True)
     query = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="history")
